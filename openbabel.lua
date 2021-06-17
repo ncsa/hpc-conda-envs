8194ca37169ca18ec8bdc46edcf41ad508fbd397
Jeff Terstriep <jefft@illinois.edu>
2021-06-17 18:43:30 -0500

    Add OpenBabel application at partner request.

---
+++ openbabel.lua
@@ -0,0 +1,24 @@
+help([[
+Open Babel is a chemical toolbox designed to speak the many languages 
+of chemical data. It's an open, collaborative project allowing anyone 
+to search, convert, analyze, or store data from molecular modeling, 
+chemistry, solid-state materials, biochemistry, or related areas.
+
+Activates the openbabel conda environment including the obabel command line
+interface.
+]])
+
+-- conda environment name
+local env="openbabel"
+
+-- extensions aid in documenting the python version provided
+extensions("python/3.9.2")
+
+-- always_load must be used because the conda3/init is unloaded (removing
+-- all the conda functions) prior to the execution of conda deactivate
+-- command when the load function is used.
+always_load("conda/init")
+
+-- use conda to activate environments to ensure proper configuration
+execute {cmd="conda activate " .. env, modeA={"load"}}
+execute {cmd="conda deactivate", modeA={"unload"}}
