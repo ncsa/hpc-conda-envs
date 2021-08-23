f080a29aa49cfa1bb1b2df08c0f9d6475f10d8d8
Jeff Terstriep <jefft@illinois.edu>
2021-08-23 10:54:50 -0500

    Improve the cleanup if module is unloaded.

--- conda/init/4.10.lua
+++ conda/init/4.10.lua
@@ -1,20 +1,32 @@
 help([[
-Loads the miniconda base environment. The base environment contains minimal 
-number of Python packages and is generally only used for building other
-conda environments.
+This module loads the conda shell functions and variables but does NOT 
+activate any conda environments. It is loaded automatically by the 
+various conda/env modules and users are encouraged to use those directly.
 ]])
 
--- conda environment name
-local env="base"
+local root = "/usr/apps/general/miniconda3"
 
--- extensions aid in documenting the python version provided
-extensions("python/3.9.2")
+-- load the conda shell functions
+local cmd = "source " .. root .. "/etc/profile.d/conda." .. myShellType()
+execute {cmd=cmd, modeA={"load"}}
 
--- always_load must be used because the conda3/init is unloaded (removing
--- all the conda functions) prior to the execution of conda deactivate
--- command when the load function is used.
-always_load("conda/init")
+-- this happens at unload
+if (mode() == "unload") then
 
--- use conda to activate environments to ensure proper configuration
-execute {cmd="conda activate " .. env, modeA={"load"}}
-execute {cmd="conda deactivate", modeA={"unload"}}
+  -- csh sets these environment variables and an alias for conda
+  if (myShellType() == "csh") then
+    cmd = "unsetenv CONDA_EXE; unsetenv _CONDA_ROOT; unsetenv _CONDA_EXE; " ..
+          "unsetenv CONDA_SHLVL; unalias conda"
+    execute{cmd=cmd, modeA={"unload"}}
+  end
+
+  -- bash sets environment variables, shell functions and path to condabin
+  if (myShellType() == "sh") then
+    remove_path("PATH", pathJoin(root,"condabin"))
+    cmd = "unset CONDA_EXE; unset CONDA_PYTHON_EXE; " ..
+        "unset _CE_CONDA; unset CONDA_SHLVL; unset -f conda; " ..
+        "unset -f __conda_activate; unset -f __conda_reactivate; " ..
+        "unset -f __conda_hashr; unset -f __add_sys_prefix_to_path"
+    execute{cmd=cmd, modeA={"unload"}}
+  end
+end
