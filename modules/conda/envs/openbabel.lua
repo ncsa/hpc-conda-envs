4b4b07813d98050374c4a2504089951f0df7fa1d
Jeff Terstriep <jefft@illinois.edu>
2022-08-26 17:30:15 -0500

    Use the miniconda3 as the base conda module.

--- modules/conda/envs/openbabel.lua
+++ modules/conda/envs/openbabel.lua
@@ -17,7 +17,7 @@ extensions("python/3.9.2")
 -- always_load must be used because the conda3/init is unloaded (removing
 -- all the conda functions) prior to the execution of conda deactivate
 -- command when the load function is used.
-always_load("conda/init")
+always_load("miniconda3")
 
 -- use conda to activate environments to ensure proper configuration
 execute {cmd="conda activate " .. env, modeA={"load"}}
--- modules/conda/envs/tensorflow-gpu-2.4.lua
+++ modules/conda/envs/tensorflow-gpu-2.4.lua
@@ -11,7 +11,7 @@ extensions("python/3.9")
 -- always_load must be used because the conda3/init is unloaded (removing
 -- all the conda functions) prior to the execution of conda deactivate
 -- command when the load function is used.
-always_load("conda/init")
+always_load("miniconda3")
 
 -- use conda to activate environments to ensure proper configuration
 execute {cmd="conda activate " .. env, modeA={"load"}}
