7b27790bd3bb1851c59e13682a3fca24566ca33d
Peter Enstrom <pmenstrom@gmail.com>
2023-04-21 09:24:55 -0500

    Added environtments for bclwfastq2, cellranger, hicup, homer, and sentieon Updated email address

---
+++ modules/conda/envs/sentieon.lua
@@ -0,0 +1,19 @@
+help([[
+Activates the sentieon conda environment.
+https://www.sentieon.com/
+]])
+
+-- conda environment name
+local env="sentieon"
+
+-- extensions aid in documenting the python version provided
+extensions("python/3.9")
+
+-- always_load must be used because the conda3/init is unloaded (removing
+-- all the conda functions) prior to the execution of conda deactivate
+-- command when the load function is used.
+always_load("miniconda3")
+
+-- use conda to activate environments to ensure proper configuration
+execute {cmd="conda activate " .. env, modeA={"load"}}
+execute {cmd="conda deactivate", modeA={"unload"}}
