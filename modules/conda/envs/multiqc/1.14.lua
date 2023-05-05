578000ababe0b7750072dbe7292723043bda2c66
Peter Enstrom <pmenstrom@gmail.com>
2023-05-05 08:23:40 -0500

    Added multiqc-1.14

---
+++ modules/conda/envs/multiqc/1.14.lua
@@ -0,0 +1,20 @@
+help([[
+Aggregate results from bioinformatics analyses across
+many samples into a single report
+https://multiqc.info/
+]])
+
+-- conda environment name
+local env="multiqc"
+
+-- extensions aid in documenting the python version provided
+extensions("python/3.8")
+
+-- always_load must be used because the conda3/init is unloaded (removing
+-- all the conda functions) prior to the execution of conda deactivate
+-- command when the load function is used.
+always_load("miniconda3")
+
+-- use conda to activate environments to ensure proper configuration
+execute {cmd="conda activate " .. env, modeA={"load"}}
+execute {cmd="conda deactivate", modeA={"unload"}}
