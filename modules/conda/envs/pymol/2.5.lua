34e12128dcc53560d33a0b206fa8e642945d890b
Jeff Terstriep <jefft@illinois.edu>
2023-05-05 15:38:02 -0500

    Add pymol from Schrodinger channel

---
+++ modules/conda/envs/pymol/2.5.lua
@@ -0,0 +1,15 @@
+help([[
+Activates the pymol conda environment.
+]])
+
+-- conda environment name
+local env="pymol-2.5"
+
+-- always_load must be used because the conda3/init is unloaded (removing
+-- all the conda functions) prior to the execution of conda deactivate
+-- command when the load function is used.
+always_load("miniconda3")
+
+-- use conda to activate environments to ensure proper configuration
+execute {cmd="conda activate --stack " .. env, modeA={"load"}}
+execute {cmd="conda deactivate", modeA={"unload"}}
