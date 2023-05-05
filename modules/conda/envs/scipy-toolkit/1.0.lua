d2ed5d1f9612167881a672b2e00e5de8ea9e393c
Jeff Terstriep <jefft@illinois.edu>
2023-05-04 20:02:19 -0500

    Add scipy-toolkit and remove old scipy environment

--- modules/conda/envs/scipy-toolkit/1.0.lua
+++ modules/conda/envs/scipy-toolkit/1.0.lua
@@ -1,13 +1,8 @@
 help([[
-Activates the scipy conda environment. This environment provides a good
-starting point for many scientific applications.
 ]])
 
 -- conda environment name
-local env="scipy"
-
--- extensions aid in documenting the python version provided
-extensions("python/3.9")
+local env="scipy-toolkit-1.0"
 
 -- always_load must be used because the conda3/init is unloaded (removing
 -- all the conda functions) prior to the execution of conda deactivate
@@ -15,5 +10,5 @@ extensions("python/3.9")
 always_load("miniconda3")
 
 -- use conda to activate environments to ensure proper configuration
-execute {cmd="conda activate " .. env, modeA={"load"}}
+execute {cmd="conda activate --stack " .. env, modeA={"load"}}
 execute {cmd="conda deactivate", modeA={"unload"}}
